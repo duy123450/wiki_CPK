42f155f12dda2c6bfe3a4a78fbce677deb9c8d73
duy123450 <duy.h0098642@gmail.com>
2026-04-08 08:02:34 +0700

    1

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,10 +1,28 @@
 const cloudinary = require('cloudinary').v2
+const {CloudinaryStorage} = require('multer-storage-cloudinary')
+const multer = require('multer')
 require('dotenv').config()
 
+// 1. Cloudinary Config
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
 })
 
-module.exports = cloudinary
+// 2. Storage Engine Setup
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'wiki-cpk',
+        allowed_formats: ['jpg', 'png', 'jpeg', 'jfif']
+    }
+})
+
+// 3. Initialize Multer
+const upload = multer({ storage: storage })
+
+module.exports = {
+    cloudinary,
+    upload
+}
