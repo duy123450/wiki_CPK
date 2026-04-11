99349025c34b41db27bf965f59a9f6315c2759ee
duy123450 <duy.h0098642@gmail.com>
2026-04-11 22:40:54 +0700

    3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,44 +11,28 @@ export default defineConfig({
   
   resolve: {
     alias: {
-      /** * Use '@' to represent your 'src' directory.
-       * This speeds up the 'rolldown:vite-resolve' process by avoiding 
-       * deep relative paths like ../../../components.
-       */
       '@': path.resolve(__dirname, './src'),
     },
   },
 
   css: {
-    /** * Switching to Lightning CSS (written in Rust) solves the 53% build time 
-     * spent in 'vite:css'. It is much faster than standard PostCSS.
-     */
     transformer: 'lightningcss',
     lightningcss: {
       targets: {
-        // Broad browser support
         safari: 14,
-        edge: 90,
-        chrome: 90,
-        firefox: 90,
       },
-    }
+    },
   },
 
   build: {
-    /** * Uses Lightning CSS for minification, further speeding up the build.
-     * Note: Ensure you have run 'npm install -D lightningcss'
-     */
     cssMinify: 'lightningcss',
-    
-    // Disabling sourcemaps speeds up build time and reduces file size
     sourcemap: false,
-    
     rollupOptions: {
       output: {
-        // Groups major libraries into a separate vendor chunk for better caching
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         },
       },
     },
@@ -62,4 +46,4 @@ export default defineConfig({
       },
     },
   },
-})
+}) 
